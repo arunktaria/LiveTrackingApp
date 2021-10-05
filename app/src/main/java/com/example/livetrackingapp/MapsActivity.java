96c27689814224207e2924fdc2b1c338f4763793
arun kumar <arunktariaa@gmail.com>
2021-10-05 13:17:06 +0530

    smooth car animation

--- app/src/main/java/com/example/livetrackingapp/MapsActivity.java
+++ app/src/main/java/com/example/livetrackingapp/MapsActivity.java
@@ -10,7 +10,12 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
+import android.os.SystemClock;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -38,7 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     int distance = 1;
     LocationManager manager;
     DatabaseReference reference;
-
+    float start_rotation;
     Marker marker;
     LatLng latLng;
     GoogleMap mmap;
@@ -108,13 +113,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void setLocationMarker(Location location) {
 
         LatLng latLng1 = new LatLng(location.getLatitude(), location.getLongitude());
-        marker.setPosition(latLng1);
-        marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.yellocar));
-        marker.setRotation(location.getBearing());
-        marker.setAnchor((float) 0.5, (float) 0.5);
-        mMap.getUiSettings().setAllGesturesEnabled(true);
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+        marker.setAnchor((float) 0.6, (float) 0.5);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng1,17f));
+        moveVechile(marker,location);
+        rotateMarker(marker,location.getBearing(),start_rotation);
+
     }
 
     //on map ready to use
@@ -122,7 +125,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mmap = googleMap;
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
         marker = mMap.addMarker(new MarkerOptions().position(latLng).title("They"));
+        marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.yellocar));
         startLocationtracking();
         Toast.makeText(MapsActivity.this, "Map is ready...", Toast.LENGTH_SHORT).show();
 
@@ -141,4 +147,89 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+
+
+
+    public void moveVechile(final Marker myMarker, final Location finalPosition) {
+
+        final LatLng startPosition = myMarker.getPosition();
+
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final Interpolator interpolator = new AccelerateDecelerateInterpolator();
+        final float durationInMs = 3000;
+        final boolean hideMarker = false;
+
+        handler.post(new Runnable() {
+            long elapsed;
+            float t;
+            float v;
+
+            @Override
+            public void run() {
+                // Calculate progress using interpolator
+                elapsed = SystemClock.uptimeMillis() - start;
+                t = elapsed / durationInMs;
+                v = interpolator.getInterpolation(t);
+
+                LatLng currentPosition = new LatLng(
+                        startPosition.latitude * (1 - t) + (finalPosition.getLatitude()) * t,
+                        startPosition.longitude * (1 - t) + (finalPosition.getLongitude()) * t);
+                myMarker.setPosition(currentPosition);
+                // myMarker.setRotation(finalPosition.getBearing());
+
+
+                // Repeat till progress is completeelse
+                if (t < 1) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                    // handler.postDelayed(this, 100);
+                } else {
+                    if (hideMarker) {
+                        myMarker.setVisible(false);
+                    } else {
+                        myMarker.setVisible(true);
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    public void rotateMarker(final Marker marker, final float toRotation, final float st) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final float startRotation = st;
+        final long duration = 1555;
+
+        final Interpolator interpolator = new LinearInterpolator();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed / duration);
+
+                float rot = t * toRotation + (1 - t) * startRotation;
+
+
+                marker.setRotation(-rot > 180 ? rot / 2 : rot);
+                start_rotation = -rot > 180 ? rot / 2 : rot;
+                if (t < 1.0) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                }
+
+            }
+        });
+    }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/livetrackingapp/TrackOthers.java
+++ app/src/main/java/com/example/livetrackingapp/TrackOthers.java
@@ -10,7 +10,12 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
+import android.os.SystemClock;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -40,10 +45,12 @@ public class TrackOthers extends FragmentActivity implements OnMapReadyCallback 
     LatLng latLng;
     GoogleMap mmap;
     Geocoder geocoder;
+    float start_rotation;
     LocationRequest locationRequest;
     FusedLocationProviderClient fusedLocationProviderClient;
-    Location location;
+
     ActivityTrackOthersBinding bind;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,11 +61,11 @@ public class TrackOthers extends FragmentActivity implements OnMapReadyCallback 
         setContentView(bind.getRoot());
 
         //for realtime database
-        reference=FirebaseDatabase.getInstance().getReference().child("UserLocation");
+        reference = FirebaseDatabase.getInstance().getReference().child("UserLocation");
 
         //taking permission from user
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 100);
 
         } else {
             Toast.makeText(this, "permission Granted!", Toast.LENGTH_SHORT).show();
@@ -71,7 +78,7 @@ public class TrackOthers extends FragmentActivity implements OnMapReadyCallback 
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        latLng = new LatLng(10, 115);
+        latLng = new LatLng(30, 76);
         geocoder = new Geocoder(this);
         reference = FirebaseDatabase.getInstance().getReference().child("UserLocation");
 
@@ -79,19 +86,19 @@ public class TrackOthers extends FragmentActivity implements OnMapReadyCallback 
 
     //getting location data from realtime database
     public void startLocationtracking() {
-    reference.addValueEventListener(new ValueEventListener() {
-        @Override
-        public void onDataChange(@NonNull DataSnapshot snapshot) {
-        DataModel location=snapshot.getValue(DataModel.class);
-       // Toast.makeText(TrackOthers.this, "start tracking...", Toast.LENGTH_SHORT).show();
-         settingLocationMarker(location);
-        }
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                DataModel location = snapshot.getValue(DataModel.class);
+                // Toast.makeText(TrackOthers.this, "start tracking...", Toast.LENGTH_SHORT).show();
+                settingLocationMarker(location);
+            }
 
-        @Override
-        public void onCancelled(@NonNull DatabaseError error) {
-            Toast.makeText(TrackOthers.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    });
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(TrackOthers.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -99,14 +106,14 @@ public class TrackOthers extends FragmentActivity implements OnMapReadyCallback 
     //setting location marker
     public void settingLocationMarker(DataModel location) {
 
-        LatLng latLng1 = new LatLng(location.getLatitude(), location.getLongitude());
-        marker.setPosition(latLng1);
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
         marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.yellocar));
-        marker.setRotation((float) location.getBearing());
-        marker.setAnchor((float) 0.5, (float) 0.5);
-        mmap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng1,18f));
-        mMap.getUiSettings().setAllGesturesEnabled(true);
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f));
+        marker.setAnchor((float) 0.6, (float) 0.5);
+
+        moveVechile(marker, location);
+        rotateMarker(marker, (float) location.getBearing(), start_rotation);
 
 
     }
@@ -116,7 +123,8 @@ public class TrackOthers extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mmap = googleMap;
-
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
         marker = mMap.addMarker(new MarkerOptions().position(latLng).title("They"));
         startLocationtracking();
         Toast.makeText(TrackOthers.this, "Map is ready...", Toast.LENGTH_SHORT).show();
@@ -125,14 +133,89 @@ public class TrackOthers extends FragmentActivity implements OnMapReadyCallback 
     //operation perform when location is granted or not
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode==100)
-        {
+        if (requestCode == 100) {
             Toast.makeText(TrackOthers.this, "permission granted!", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
+        } else {
             Toast.makeText(TrackOthers.this, "permission Denied!", Toast.LENGTH_SHORT).show();
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+
+    public void moveVechile(final Marker myMarker, final DataModel finalPosition) {
+
+        final LatLng startPosition = myMarker.getPosition();
+
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final Interpolator interpolator = new AccelerateDecelerateInterpolator();
+        final float durationInMs = 3000;
+        final boolean hideMarker = false;
+
+        handler.post(new Runnable() {
+            long elapsed;
+            float t;
+            float v;
+
+            @Override
+            public void run() {
+                // Calculate progress using interpolator
+                elapsed = SystemClock.uptimeMillis() - start;
+                t = elapsed / durationInMs;
+                v = interpolator.getInterpolation(t);
+
+                LatLng currentPosition = new LatLng(
+                        startPosition.latitude * (1 - t) + (finalPosition.getLatitude()) * t,
+                        startPosition.longitude * (1 - t) + (finalPosition.getLongitude()) * t);
+                myMarker.setPosition(currentPosition);
+                // myMarker.setRotation(finalPosition.getBearing());
+
+
+                // Repeat till progress is completeelse
+                if (t < 1) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                    // handler.postDelayed(this, 100);
+                } else {
+                    if (hideMarker) {
+                        myMarker.setVisible(false);
+                    } else {
+                        myMarker.setVisible(true);
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    public void rotateMarker(final Marker marker, final float toRotation, final float st) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final float startRotation = st;
+        final long duration = 1555;
+
+        final Interpolator interpolator = new LinearInterpolator();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed / duration);
+
+                float rot = t * toRotation + (1 - t) * startRotation;
+
+
+                marker.setRotation(-rot > 180 ? rot / 2 : rot);
+                start_rotation = -rot > 180 ? rot / 2 : rot;
+                if (t < 1.0) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                }
+
+            }
+        });
+    }
+
+
 }
